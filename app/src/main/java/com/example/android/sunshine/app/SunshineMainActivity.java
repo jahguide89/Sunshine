8d793d8917f6a5f648efc916b757a2ea11a286a3
IVSA-ASUS <IVSA-ASUS@192.168.1.15>
2015-05-31 00:46:25 +0100

    Finished lesson 2

--- app/src/main/java/com/example/android/sunshine/app/SunshineMainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SunshineMainActivity.java
@@ -12,6 +12,11 @@ public class SunshineMainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sunshine_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
     }
 
 
